8fe694bdd5cba393f5735d7690f07e7851a14bde
John Sims <simsjack30@gmail.com>
2024-03-21 15:03:02 -0500

    Test Commit

--- Assignment2/ContentView.swift
+++ Assignment2/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Jack!")
+            Text("Hello, Jack! Testing")
         }
         .padding()
     }
