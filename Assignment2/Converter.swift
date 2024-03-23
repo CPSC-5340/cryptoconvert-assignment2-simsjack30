fc29e0a3f08654027704e3e22a8ef6e29cbf7e13
simsjack30 <simsjack30@gmail.com>
2024-03-23 14:24:21 -0500

    Video 1 follow along

---
+++ Assignment2/Converter.swift
@@ -0,0 +1,29 @@
+//
+// ContentView.swift : Assignment2
+//
+// Copyright © 2023 Auburn University.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+struct Converter: View {
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))]) {
+                CurrencyItem()
+                CurrencyItem()
+                CurrencyItem()
+                CurrencyItem()
+                CurrencyItem()
+                CurrencyItem()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Converter()
+    }
+}
---
+++ Assignment2/CurrencyItem.swift
@@ -0,0 +1,67 @@
+//
+//  CurrencyItem.swift
+//  Assignment2
+//
+//  Created by John Sims on 3/23/24.
+//
+
+import SwiftUI
+
+struct CurrencyItem: View {
+    
+    @State var isFront = true
+    
+    var body: some View {
+        ZStack {
+            if isFront {
+                FrontCurrencyItem()
+            } else {
+                BackCurrencyItem()
+            }
+        }.onTapGesture {
+            isFront.toggle()
+        }
+    }
+}
+
+struct CurrencyItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrencyItem()
+    }
+}
+
+struct FrontCurrencyItem: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("🇺🇸")
+                Spacer()
+            }
+            HStack {
+                Spacer()
+                Text("USD")
+            }
+        }
+        .frame(width: 130, height: 130)
+        .padding()
+        .background(.blue)
+    }
+}
+
+struct BackCurrencyItem: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("US Dollar")
+                Spacer()
+            }
+            HStack {
+                Spacer()
+                Text(String(1))
+            }
+        }
+        .frame(width: 130, height: 130)
+        .padding()
+        .background(.blue)
+    }
+}
